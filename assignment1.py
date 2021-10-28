b1c405b39cf6141a9d67797d4bd0d90346a24b4e
Emiria <Emiriachan@users.noreply.github.com>
2021-10-28 13:46:55 +0800

    completed a1

--- assignment1.py
+++ assignment1.py
@@ -11,41 +11,32 @@ def count_requirement():
         reader = csv.reader(albums)
         requirement = 0
         for row in reader:
-            if row[0] == "r":
+            if row[3] == "r":
                 requirement += 1
     return requirement
 
 
-def input_int_positive_number():
+def input_int_positive_number(data_name):
     while True:
-        number = input(">>> ")
+        number = input(data_name)
         if not number.isnumeric():
-            print("Valid Number")
+            print("Enter a number")
             continue
-        if number.count('.') == 1:
-            new_s = number.split('.')
-            left_num = new_s[0]
-            right_num = new_s[1]
-            if right_num.isdigit():
-                if left_num.isdigit():
-                    print("Valid Number")
-                    continue
-                elif left_num.count('-') == 1 and left_num.startswith('-'):
-                    tmp_num = left_num.split('-')[-1]
-                    if tmp_num.isdigit():
-                        print("Valid Number")
-                        continue
+        elif number.count('.') == 1:
+            print("Enter an integrate number")
         elif number.count(".") == 0:
             if number.isdigit():
-                return int(number)
-            elif number.count('-') == 1 and number.startswith('-'):
-                ss = number.split('-')[-1]
-                if ss.isdigit():
-                    print("Valid Number")
+                if int(number) > 0:
+                    return int(number)
+                else:
+                    print("Enter a positive number")
                     continue
+        else:
+            print("Valid input")
+            continue
 
 
-def list_albums():
+def list_albums(choice):
     with open("albums.csv", "r") as albums:
         reader = csv.reader(albums)
         requirement = 0
@@ -56,7 +47,14 @@ def list_albums():
                 print(" {}. {:<30s}by {:<15}({})".format(index + 1, row[0], row[1], row[2]))
             if row[3] == "r":
                 requirement += 1
-        print(f"You need to listen to {requirement} albums.")
+        if count_requirement() == 0:
+            if choice == "l":
+                print("No albums left to listen to. Why not add a new album?")
+            else:
+                print("No required albums")
+                return True
+        else:
+            print(f"You need to listen to {requirement} albums.")
 
 
 def main():
@@ -67,9 +65,7 @@ def main():
         print("Menu:\nL - List all albums\nA - Add new album\nM - Mark an album as completed\nQ - Quit")
         choice = input(">>> ")
         if choice == "L" or choice == "l":
-            list_albums()
-            if count_requirement() == 0:
-                print("No albums left to listen to. Why not add a new album?")
+            list_albums("l")
         elif choice == "A" or choice == "a":
             title = input("Title: ")
             while title.isspace():
@@ -79,24 +75,17 @@ def main():
             while artist.isspace():
                 print("can not be blank")
                 artist = input("Artist: ")
-            years = input_int_positive_number()
-            add_row = []
-            add_row[0] = title
-            add_row[1] = artist
-            add_row[2] = years
-            add_row[3] = "r"
+            years = input_int_positive_number("years: ")
+            add_row = [title, artist, years, "r"]
             with open("albums.csv", "a", newline="") as albums:
                 writer = csv.writer(albums)
                 writer.writerow(add_row)
             print("{} by {} ({}) added to Album Tracker".format(title, artist, years))
         elif choice == "M" or choice == "m":
-
-            if count_requirement() == 0:
-                print("No required albums")
+            if list_albums("m"):
                 continue
-            list_albums()
             print("Enter the number of an album to mark as completed")
-            completed_album = input_int_positive_number()
+            completed_album = input_int_positive_number(">>> ")
             with open("albums.csv", "r") as albums:
                 reader = csv.reader(albums)
                 li = list(reader)
